82a0c71c8dffcf4f7880c1a9f2fa79f728de807c
mrsekut <k.cloudspider@gmail.com>
2021-06-07 02:29:25 +0900

    feat(zshrc): main install

---
+++ zsh/0-main/install.sh
@@ -0,0 +1,17 @@
+# catの代替
+brew install bat
+
+# curlの代替
+brew install httpie
+
+# 行数を表示する
+brew install cloc
+
+# repository管理
+brew install ghq
+
+# tree形式でディレクトリを表示
+brew install tree
+
+# formatter
+brew install jq
---
+++ zsh/haskell/install.sh
@@ -0,0 +1,2 @@
+# linter
+brew install hlint
