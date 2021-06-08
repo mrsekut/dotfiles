5ce1d833b190d807f5dc9d61a4bb9b9a8b7170f1
mrsekut <k.cloudspider@gmail.com>
2021-06-09 02:59:53 +0900

    feat: vscode settings

---
+++ vscode/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
+
+rm "$VSCODE_SETTING_DIR/settings.json"
+ln -s "$SCRIPT_DIR/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
+
+rm "$VSCODE_SETTING_DIR/keybindings.json"
+ln -s "$SCRIPT_DIR/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
+
+cat ./extensions | while read line
+do
+ code --install-extension $line
+done
+
+code --list-extensions > extensions
