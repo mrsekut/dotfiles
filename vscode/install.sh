9d441a638504092a3193d9d8bf545a0feace877d
mrsekut <k.cloudspider@gmail.com>
2021-06-09 16:49:54 +0900

    VSCodeのisntall.shでbackupを取るように修正

--- vscode/install.sh
+++ vscode/install.sh
@@ -2,6 +2,13 @@
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
 
+# backup
+mkdir "$SCRIPT_DIR/backup"
+cp "$VSCODE_SETTING_DIR/settings.json" "$SCRIPT_DIR/backup"
+cp "$VSCODE_SETTING_DIR/keybindings.json" "$SCRIPT_DIR/backup"
+code --list-extensions > "$SCRIPT_DIR/backup/extensions"
+
+
 rm "$VSCODE_SETTING_DIR/settings.json"
 ln -s "$SCRIPT_DIR/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
 
