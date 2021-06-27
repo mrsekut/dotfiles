3fc5fe6c7e8d0511b7feab432870a1c70cbe34f2
mrsekut <k.cloudspider@gmail.com>
2021-06-27 18:42:11 +0900

    feat: add starship

--- nix/home.nix
+++ nix/home.nix
@@ -10,6 +10,8 @@
   home.packages = with pkgs; [
     bat
     exa
+    cloc
+    starship
 
     # jq
     # ghq
@@ -45,9 +47,10 @@
 
   programs.zsh = {
     enable = true;
-    # autocd = true;
-    # enableCompletion = true;
-    # enableAutosuggestions = true;
+    # TODO: この辺の意味
+    autocd = true;
+    enableCompletion = true;
+    enableAutosuggestions = true;
     shellAliases = {
       cdd = "cd $(ghq list --full-path | fzf) && code .";
       cdg = "cd $(ghq list --full-path | fzf)";
@@ -56,6 +59,40 @@
       ls = "exa";
       cat = "bat";
       sed = "gsed";
+    };
+    enable = true;
+    # plugins = [
+    #   {
+    #     name = "fast-syntax-highlighting";
+    #     src = pkgs.zsh-fast-syntax-highlighting.src;
+    #   }
+    #   {
+    #     name = "history-search-multi-word";
+    #     src = pkgs.fetchFromGitHub {
+    #       owner = "zdharma";
+    #       repo = "history-search-multi-word";
+    #       rev = "v1.2.52";
+    #       sha256 = "1dqwkw3cwvmmmaczs7vrh3wgrxhc9s2vbyn56nk9rc3561s0s9w7";
+    #     };
+    #   }
+    #   {
+    #     name = "zsh-completions";
+    #     src = pkgs.zsh-completions.src;
+    #   }
+    #   {
+    #     name = "nix-zsh-completions";
+    #     src = pkgs.nix-zsh-completions.src;
+    #   }
+    # ];
+  };
+
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      character = {
+        success_symbol = "λ";
+      };
     };
   };
 }
