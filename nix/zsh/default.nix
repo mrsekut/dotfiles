99698498b08b1116e98b2a4ec8f3723b9141be15
mrsekut <k.cloudspider@gmail.com>
2021-06-29 02:22:54 +0900

    feat: enhancd不要

--- nix/zsh/default.nix
+++ nix/zsh/default.nix
@@ -16,19 +16,6 @@
     #   enable = true;
     # };
 
-    plugins = [
-      {
-        name = "enhancd";
-        file = "init.sh";
-        src = pkgs.fetchFromGitHub {
-          owner = "b4b4r07";
-          repo = "enhancd";
-          rev = "aec0e0c1c0b1376e87da74b8940fda5657269948";
-          sha256 = "13n2c2kj25g8aqvlkb5j4vzcz5a4a22yc8v6ary651lpqgckx7cg";
-        };
-      }
-    ];
-
     initExtra = ''
       source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
