f7b33fbdfd010ffe28989aafc38d46c7f15ed082
mrsekut <k.cloudspider@gmail.com>
2021-06-28 22:47:50 +0900

    feat: add enhancd

--- nix/zsh/default.nix
+++ nix/zsh/default.nix
@@ -12,6 +12,22 @@
     enable = true;
     enableAutosuggestions = true;
 
+    # oh-my-zsh = {
+    #   enable = true;
+    # };
+
+    plugins = [
+      {
+        name = "enhancd";
+        file = "init.sh";
+        src = pkgs.fetchFromGitHub {
+          owner = "b4b4r07";
+          repo = "enhancd";
+          rev = "aec0e0c1c0b1376e87da74b8940fda5657269948";
+          sha256 = "13n2c2kj25g8aqvlkb5j4vzcz5a4a22yc8v6ary651lpqgckx7cg";
+        };
+      }
+    ];
 
     initExtra = ''
       source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
@@ -23,6 +39,9 @@
 
       # vim for zsh
       bindkey -v
+
+      # my modular dotfiles
+      source "$HOME/Desktop/dev/github.com/mrsekut/dotfiles/zsh/export.zsh"
     '';
   };
 }
