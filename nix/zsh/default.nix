a443d44eacedb4bf7f64583a25ac05f42a9fedbe
mrsekut <k.cloudspider@gmail.com>
2021-06-30 23:14:41 +0900

    feat: zshの設定をnixに移動

--- nix/zsh/default.nix
+++ nix/zsh/default.nix
@@ -19,16 +19,31 @@
     initExtra = ''
       source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
+
       # zsh-history-substring-search
       source ${pkgs.zsh-history-substring-search}/share/zsh-history-substring-search/zsh-history-substring-search.zsh
       bindkey '^[[A' history-substring-search-up
       bindkey '^[[B' history-substring-search-down
 
+
       # vim for zsh
       bindkey -v
 
+      # emacs for zsh
+      bindkey -e
+
+
       # my modular dotfiles
       source "$HOME/Desktop/dev/github.com/mrsekut/dotfiles/zsh/export.zsh"
+
+
+      # ctrl-rでhistoryをfzfを使って表示
+      function select-history() {
+        BUFFER=$(history -n -r 1 | fzf --no-sort +m --query "$LBUFFER" --prompt="History > ")
+        CURSOR=$#BUFFER
+      }
+      zle -N select-history
+      bindkey '^r' select-history
     '';
   };
 }
--- zsh/zsh/install.zsh
+++
@@ -1,6 +0,0 @@
-
-brew install zsh
-
-
-# oh-my-zsh
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
