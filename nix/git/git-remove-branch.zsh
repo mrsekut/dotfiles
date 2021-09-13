ec10f4284c97c66ce577cf13f3eb959f93a8e60a
mrsekut <k.cloudspider@gmail.com>
2021-09-14 00:31:32 +0900

    feat: `git bd`の結果にmerge済みなら`M`を付ける

--- nix/git/git-remove-branch.zsh
+++ nix/git/git-remove-branch.zsh
@@ -2,15 +2,28 @@
 
 # fzfを使って`git branch -d`を実行
 # ctrl-iで選択し、`ctrl-d`で削除を実行
+# developにmerge済みのものは`M`を付ける
 
-while out=$(git branch |
-  sed -e '/^\*/d' |
-  fzf --multi --reverse --print-query --expect=ctrl-d); do
+show_branches() {
+  (
+    git branch --merged develop |
+      sed -e '/^\*/d' |                 # exclude current branch
+      sed -E '/develop|master/d' |      # exclude develop and master
+      sed -e 's|^ | M|'                 # marked merged
+    git branch --no-merged develop |
+      sed -e '/^\*/d' |                 # exclude current branch
+      sed -e 's|^ |  |'                 # formatting
+  ) |
+    cat |
+    fzf --multi --reverse --print-query --expect=ctrl-d
+}
+
+while out=$(show_branches); do
   k=$(head -2 <<<"$out" | tail -1)
 
   if [ "$k" = ctrl-d ]; then
     sed '1,2d' <<<"$out" |
-      awk '{print $1}' |
+      sed -E 's/^ M|  //' |
       while read -r branch; do
         git branch -D $branch
         if [ $? -eq 0 ]; then
