4cbee083d998f9e6b3fb233e6eb5c55e60514aed
mrsekut <k.cloudspider@gmail.com>
2021-12-04 01:13:12 +0900

    fix: fix

--- nix-darwin/keyboard.nix
+++ nix-darwin/keyboard.nix
@@ -2,14 +2,8 @@
 
 {
   system.defaults.NSGlobalDomain = {
-    # allow key repeat
-    ApplePressAndHoldEnabled = false;
-    # delay before repeating keystrokes
     InitialKeyRepeat = 10;
-    # delay between repeated keystrokes upon holding a key
     KeyRepeat = 2;
-    AppleShowAllExtensions = true;
-    AppleShowScrollBars = "Automatic";
   };
 
   # system.keyboard = {
