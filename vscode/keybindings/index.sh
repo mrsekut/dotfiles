3a0524f33cd99590811ef93a430bde627b81fcc6
mrsekut <k.cloudspider@gmail.com>
2024-07-05 19:15:49 +0900

    keybindings

---
+++ vscode/keybindings/index.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+CURRENT=$(cd $(dirname $0) && pwd)
+VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
+BACKUP_FILE="$CURRENT/keybindings.json"
+
+backup() {
+  if [ -f "$VSCODE_SETTING_DIR/keybindings.json" ]; then
+    cp "$VSCODE_SETTING_DIR/keybindings.json" "$BACKUP_FILE"
+    echo "keybindings: Backup created"
+  fi
+}
+
+create_symlink() {
+  rm -f "$VSCODE_SETTING_DIR/keybindings.json"
+  ln -s "$CURRENT/keybindings.json" "$VSCODE_SETTING_DIR/keybindings.json"
+  echo "keybindings: Symlink created"
+}
+
+backup && create_symlink
