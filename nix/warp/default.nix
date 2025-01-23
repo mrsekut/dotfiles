9d13c23be52969d4a41bb2b9b69aa604c5866d4c
mrsekut <k.cloudspider@gmail.com>
2025-01-23 17:05:11 +0900

    hotkeyはraycastのものを使う

--- nix/warp/default.nix
+++ nix/warp/default.nix
@@ -1,9 +1,9 @@
 { lib, ... }:
 let
-  target = name: ".config/karabiner/assets/complex_modifications/${name}.json";
+  # target = name: ".config/karabiner/assets/complex_modifications/${name}.json";
 in {
-  home.file.karabinerWarpHotkey = {
-    target = target "warp-hotkey";
-    text = builtins.toJSON (lib.importJSON ./warp-hotkey.json);
-  };
+  # home.file.karabinerWarpHotkey = {
+  #   target = target "warp-hotkey";
+  #   text = builtins.toJSON (lib.importJSON ./warp-hotkey.json);
+  # };
 }
