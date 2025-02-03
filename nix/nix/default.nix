22ca203e83b1e4b26a9363b96aebe36a9b275e3a
mrsekut <k.cloudspider@gmail.com>
2025-02-03 22:36:14 +0900

    refactor: move to nix/

--- nix/nix/default.nix
+++ nix/nix/default.nix
@@ -1,16 +1,9 @@
 { pkgs, ... }:
 
 {
-  nix = {
-    package = pkgs.nixVersions.stable;
-    settings = {
-      experimental-features = ''
-        nix-command flakes
-      '';
-    };
-  };
-
-  home.packages = with pkgs; [
-    nix-prefetch-git
+  imports = [
+    ./nix
+    ./direnv
+    ./devbox
   ];
 }
