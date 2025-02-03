b3701e3e806cfade614e95d0376ceefd22fe564c
mrsekut <k.cloudspider@gmail.com>
2025-02-03 22:34:16 +0900

    使用していない設定を削除

--- nix/nix/default.nix
+++ nix/nix/default.nix
@@ -13,14 +13,4 @@
   home.packages = with pkgs; [
     nix-prefetch-git
   ];
-
-  programs.zsh = {
-    initExtra = ''
-      export NIX_PATH=$HOME/.nix-defexpr/channels:$NIX_PATH
-
-      # nix-darwin
-      export PATH=/run/current-system/sw/bin:$PATH
-      export NIX_PATH=darwin-config=$HOME/.nixpkgs/darwin-configuration.nix:$NIX_PATH
-    '';
-  };
 }
