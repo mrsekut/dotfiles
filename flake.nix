b70925d697fb65ba7afee25525ce6404aec6ebe1
mrsekut <k.cloudspider@gmail.com>
2025-02-28 11:10:46 +0900

    update: refresh home-manager version and repository reference in flake configuration

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
