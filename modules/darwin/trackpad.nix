b04ff07345321e3e718b4078c8440d92beb2d73c
mrsekut <k.cloudspider@gmail.com>
2025-03-31 15:00:08 +0900

    add comments

--- modules/darwin/trackpad.nix
+++ modules/darwin/trackpad.nix
@@ -2,11 +2,11 @@
 
 {
   system.defaults.trackpad = {
-    Clicking = true;
-    Dragging = false;
-    ActuationStrength = 1;
-    FirstClickThreshold = 0;
-    SecondClickThreshold = 0;
-    TrackpadRightClick = true;
+    Clicking = true;               # タップでクリックを有効化
+    Dragging = false;              # タップでドラッグを無効化
+    ActuationStrength = 1;         # サイレントクリックを無効（0で有効、1で無効）
+    FirstClickThreshold = 0;       # 通常クリックのクリック圧：0=軽い, 1=普通, 2=重い
+    SecondClickThreshold = 0;      # 強めのクリック（Force Click）のクリック圧：0=軽い, 1=普通, 2=重い
+    TrackpadRightClick = true;     # トラックパッドで右クリックを有効
   };
 }
