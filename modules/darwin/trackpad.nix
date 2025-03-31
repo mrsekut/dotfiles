80a49264dcc52cdbf4baa7891e856cb3c37b4cec
mrsekut <k.cloudspider@gmail.com>
2025-03-31 15:00:19 +0900

    add mouse scaling

--- modules/darwin/trackpad.nix
+++ modules/darwin/trackpad.nix
@@ -9,4 +9,6 @@
     SecondClickThreshold = 0;      # 強めのクリック（Force Click）のクリック圧：0=軽い, 1=普通, 2=重い
     TrackpadRightClick = true;     # トラックパッドで右クリックを有効
   };
+
+  system.defaults.NSGlobalDomain."com.apple.trackpad.scaling" = 3.0; # 軌跡の速さ（マウス＆トラックパッドのカーソル移動速度）
 }
