6a56871c08a8bf00f9930adb9ea2d03b7469836a
mrsekut <k.cloudspider@gmail.com>
2025-07-23 19:46:54 +0900

    update claude code

--- modules/claude/override.nix
+++ modules/claude/override.nix
@@ -1,9 +1,9 @@
 { claude-code, fetchzip }:
 claude-code.overrideAttrs (oldAttrs: rec {
-  version = "1.0.51";
+  version = "1.0.58";
   src = fetchzip {
     url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-    hash = "sha256-sAILRsi8ZViMfcpqykfnFQzHTJHRwRSZz45otMqa4U0=";
+    hash = "sha256-Mp3S269iifNGSEz83IF6bqbgdy6Im1bQjR8oaaL3W8c=";
   };
   npmDepsHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
 })
