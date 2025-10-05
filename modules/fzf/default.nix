5cdf3fb1e85f6e7955d24cacced63a57a9dbd416
mrsekut <k.cloudspider@gmail.com>
2025-10-05 17:53:54 +0900

    使ってないので削除

--- modules/fzf/default.nix
+++ modules/fzf/default.nix
@@ -12,7 +12,6 @@
     shellAliases = {
       cdd = "cd ~/Desktop";
       cdg = "cd $(ghq list --full-path | fzf)";
-      codeg = "ghq list --full-path | fzf | xargs code";
       mkcd = ''f(){ mkdir -p "$1"; cd "$1" }; f'';
     };
   };
