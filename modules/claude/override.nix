85759af10ff4cc1397616841b72b587c4137a806
mrsekut <k.cloudspider@gmail.com>
2025-11-12 10:58:16 +0900

    色々fix

--- modules/claude/override.nix
+++ modules/claude/override.nix
@@ -5,5 +5,5 @@ claude-code.overrideAttrs (oldAttrs: rec {
     url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
     hash = "sha256-SDXYXEb7Vi150Hx04y9kd+Owzu11VSqVRgej36P2khU=";
   };
-  npmDepsHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  npmDepsHash = "sha256-cPy2xudIH1OVVzDlq5YAg1uFu59h6zDOvXpKaJ+fuP8=";
 })
